90557f7f9e26ff7fb0710a0b337b8b5c84e26518
virus <virusbb001a@gmail.com>
2018-08-25 14:29:26 +0900

    change port and cors setting

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,19 @@ import express from "express";
 
 const app = express();
 
+app.set("port", (process.env.PORT || 10810));
+
+// CORS Setting
+app.use(function(_req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.get("/", function(_req, res) {
   res.send("hello world");
 });
 
-app.listen(10810);
+app.listen(app.get("port"), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
