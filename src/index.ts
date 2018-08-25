112955d935cf2cc82b48c5bea490bda6c5b9f9fe
virus <virusbb001a@gmail.com>
2018-08-25 14:39:01 +0900

    add dummy quiz, quiz interface and [GET] /quiz

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,39 @@ const app = express();
 
 app.set("port", (process.env.PORT || 10810));
 
+interface Quiz {
+  question: string;
+  choices: string[];
+  correct: number;
+  commentary: string;
+}
+
+interface Quizzes {
+  quizzes: Quiz[];
+}
+
+const questions: Quizzes = {
+  "quizzes": [{
+    question: "問題文",
+    choices: [
+      "選択肢1",
+      "選択肢2",
+      "選択肢3",
+      "選択肢4"
+    ],
+    correct: 1,
+    commentary: "解説文"
+  }, {
+    question: "○×クイズ問題文",
+    choices: [
+      "○",
+      "×"
+    ],
+    correct: 0, // 🙆
+    commentary: "解説文"
+  }]
+}
+
 // CORS Setting
 app.use(function(_req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -13,6 +46,10 @@ app.use(function(_req, res, next) {
 
 app.get("/", function(_req, res) {
   res.send("hello world");
+});
+
+app.get("/quiz", function(_req, res) {
+  res.json(questions);
 });
 
 app.listen(app.get("port"), function() {
